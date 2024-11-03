dd4a765934e72d9685921902b89830f064e409a4
eschmiel <eschmiel@gmail.com>
2024-11-03 01:01:35 -0400

    Learn about people button. Hook up github button

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,24 @@ export const Footer = () => {
     // <Container  sx={{bottom:'0px'}}>
         <Stack justifyContent='space-between' direction="row" alignItems='flex-end' sx={{bottom: '0px'}}>
             <Button variant="schmiel">SCHMIEL</Button>
-            <Stack justifyContent='space-around' width="100px" direction="row">
+            <Stack justifyContent='space-evenly' width='400px' direction="row" alignItems='flex-end'>
+                <Button sx={{
+                    height:"60px", 
+                    width:"230px", 
+                    padding:"15px",
+                    borderRadius:'20px 20px 0px 0px', 
+                    border: `3px solid ${drawerBGColor}`, 
+                    [":hover"]: {height: '100px', width: '270px', color:'black' },
+                    transition: 'height .2s, width .2s',
+                    alignItems: 'center', 
+                    justifyContent: "center", 
+                    fontWeight: 'bold',
+                    fontSize: '1rem',
+                    color:colors.drawerBGColor
+                }}
+                >
+                    Learn About People
+                </Button>
                 <Button sx={{
                     height:"60px", 
                     width:"60px", 
@@ -16,7 +33,9 @@ export const Footer = () => {
                     border: `3px solid ${drawerBGColor}`, 
                     [":hover"]: {height: '100px', width: '100px'},
                     transition: 'height .2s, width .2s'
-                }}>
+                }}
+                    href="https://github.com/eschmiel/whereDoYouWantToLive"
+                >
                     <img src="./oldManZelda.png" height= "90%" width="90%"/>
                 </Button>
             </Stack>
